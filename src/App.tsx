3da9634c2f5e9ead0333d16a756b1305f9bd21ee
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-02-19 16:23:18 +0900

    feat(Header): Header 레이아웃 구성 추가
    See also: Nomadcoder React-master 9.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+import Header from "./Components/Header";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
